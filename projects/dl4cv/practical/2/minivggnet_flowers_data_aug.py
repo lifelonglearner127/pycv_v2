3b893b34aca2be7ab7694c0e07c9aaa632629d16
lifelonglearner127 <life.long.learner127@outlook.com>
2020-02-22 23:52:11 +0900

    dl4cv; practical bundle; 2; data augmentation

---
+++ projects/dl4cv/practical/2/minivggnet_flowers_data_aug.py
@@ -0,0 +1,85 @@
+# USAGE
+# python minivggnet_flowers_data_aug.py --dataset ~/datasets/flowers \
+# --output with.png
+
+import argparse
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+from imutils import paths
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from pycv.datasets import DatasetLoader
+from pycv.networks import MiniVGGNet
+from pycv.preprocessors import AspectAwarePreProcessor
+from pycv.preprocessors import ImageToArrayPreProcessor
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+                help="path to input dataset")
+ap.add_argument("-o", "--output", required=True,
+                help="path to output loss/accuracy plot")
+args = vars(ap.parse_args())
+
+image_paths = list(paths.list_images(args["dataset"]))
+class_names = [image_path.split(os.path.sep)[-2] for image_path in image_paths]
+class_names = [str(x) for x in np.unique(class_names)]
+
+aap = AspectAwarePreProcessor(64, 64)
+iap = ImageToArrayPreProcessor()
+dataset_loader = DatasetLoader(preprocessors=[aap, iap])
+(data, labels) = dataset_loader.load(image_paths, verbose=500)
+data = data.astype("float") / 255.0
+
+(train_X, test_X, train_y, test_y) = train_test_split(
+    data, labels, test_size=0.25, random_state=42
+)
+train_y = LabelBinarizer().fit_transform(train_y)
+test_y = LabelBinarizer().fit_transform(test_y)
+
+aug = ImageDataGenerator(
+    rotation_range=30, width_shift_range=0.1, height_shift_range=0.1,
+    shear_range=0.2, zoom_range=0.2, horizontal_flip=True, fill_mode="nearest"
+)
+
+print("[INFO] compiling model...")
+sgd = SGD(lr=0.05)
+model = MiniVGGNet.build(
+    width=64, height=64, depth=3, classes=len(class_names)
+)
+model.compile(
+    loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"]
+)
+
+print("[INFO] training network...")
+H = model.fit(aug.flow(train_X, train_y, batch_size=32),
+              validation_data=(test_X, test_y),
+              steps_per_epoch=len(train_X) // 32,
+              epochs=100, verbose=1)
+
+print("[INFO] evaluating network...")
+predictions = model.predict(test_X, batch_size=32)
+print(
+    classification_report(
+        test_y.argmax(axis=1),
+        predictions.argmax(axis=1),
+        target_names=class_names
+    )
+)
+
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, 100), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, 100), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, 100), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, 100), H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend()
+plt.savefig(args["output"])
+plt.show()
--- pycv/preprocessors.py
+++ pycv/preprocessors.py
@@ -1,4 +1,5 @@
 import cv2
+import imutils
 from tensorflow.keras.preprocessing.image import img_to_array
 
 
@@ -21,3 +22,31 @@ class ImageToArrayPreProcessor:
 
     def preprocess(self, image):
         return img_to_array(image, data_format=self.data_format)
+
+
+class AspectAwarePreProcessor:
+
+    def __init__(self, width, height, interpolation=cv2.INTER_AREA):
+        self.width = width
+        self.height = height
+        self.interpolation = interpolation
+
+    def preprocess(self, image):
+        (h, w) = image.shape[:2]
+        d_w = 0
+        d_h = 0
+
+        if w < h:
+            image = imutils.resize(image, width=self.width,
+                                   inter=self.interpolation)
+            d_h = int((image.shape[0] - self.height) / 2.0)
+        else:
+            image = imutils.resize(image, height=self.height,
+                                   inter=self.interpolation)
+            d_w = int((image.shape[1] - self.width) / 2.0)
+
+        (h, w) = image.shape[:2]
+        image = image[d_h:h - d_h, d_w:w - d_w]
+
+        return cv2.resize(image, (self.width, self.height),
+                          interpolation=self.interpolation)
